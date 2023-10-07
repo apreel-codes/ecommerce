7ae6463c625509e5fb6d4a2f1138581298a73d32
Apreel-hub <83452532+Apreel-hub@users.noreply.github.com>
2023-10-07 14:38:55 +0100

    project configuration

--- server/server.js
+++ server/server.js
@@ -3,9 +3,20 @@ require('dotenv').config();
 import express from 'express';
 const app = express()
 
-app.get('/api', (req, res) => {
-   
+app.get('/', (req, res) => {
+   console.log('This is home page')
 })
 
 app.listen(5000, () => {console.log("Server is listening on Port 5000")
 })
+
+// '/'
+// '/collections'
+// '/collections/brand'
+// '/collections/brand/products'
+// '/collections/brand/products/productName'
+// '/collections/brand/products/productName?variant=productVariantId'
+// '/checkout'
+// '/register'
+// '/login'
+// '/logout'
